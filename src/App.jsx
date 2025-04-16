1bcf96738e685bd6c662a035d070a4356b5eb749
sajidali <mohammedsajid@gmail.com>
2025-04-16 17:18:59 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import CollegeDashboard from './Components/Dashboard'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UploadLogo from './Components/Uploadlogo';
 import UploadImage from './Components/Uplpadimage';
-import UploadLogos from './Components/xyz';
-import CollegeManager from './Components/xyz';
 
 function App() {
   return (
